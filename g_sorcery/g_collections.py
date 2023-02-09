18a3cc0bf4172e949f9740647702651424aa74e1
Markus Walter <houseofsuns@houseofsuns.org>
2023-02-09 13:56:15 +0100

    Add Version container.

--- g_sorcery/g_collections.py
+++ g_sorcery/g_collections.py
@@ -15,9 +15,11 @@
     :license: GPL-2, see LICENSE for more details.
 """
 
+import functools
 import re
 
 import portage
+
 
 class elist(list):
     '''Custom list type which adds a customized __str__()
@@ -183,3 +185,111 @@ class Dependency(object):
 
         return Dependency(category, package, version, operator, usedep,
                           useflag)
+
+
+@functools.total_ordering
+class Version(object):
+    """
+    Class to store a version.
+    """
+
+    __slots__ = ('formatted', 'components', 'suffix', 'alpha', 'beta', 'pre',
+                 'rc', 'p', 'revision')
+
+    def __init__(self, components, suffix="", alpha=None, beta=None, pre=None,
+                 rc=None, p=None, revision=None):
+        formatted = f'{".".join(map(str, components))}{suffix}'
+        if alpha is not None:
+            formatted = f'{formatted}_alpha{alpha}'
+        if beta is not None:
+            formatted = f'{formatted}_beta{beta}'
+        if pre is not None:
+            formatted = f'{formatted}_pre{pre}'
+        if rc is not None:
+            formatted = f'{formatted}_rc{rc}'
+        if p is not None:
+            formatted = f'{formatted}_p{p}'
+        if revision is not None:
+            formatted = f'{formatted}-r{revision}'
+        object.__setattr__(self, "formatted", formatted)
+        object.__setattr__(self, 'components', components)
+        object.__setattr__(self, 'suffix', suffix)
+        object.__setattr__(self, 'alpha', alpha)
+        object.__setattr__(self, 'beta', beta)
+        object.__setattr__(self, 'pre', pre)
+        object.__setattr__(self, 'rc', rc)
+        object.__setattr__(self, 'p', p)
+        object.__setattr__(self, 'revision', revision)
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Version instances are immutable",
+                             self.__class__, name, value)
+
+    def __str__(self):
+        return self.formatted
+
+    def __eq__(self, other):
+        if not isinstance(other, Version):
+            return NotImplemented
+        # Treat missing components as zero
+        l = max(len(self.components), len(other.components))
+        sc = self.components + ((0,) * (l - len(self.components)))
+        oc = other.components + ((0,) * (l - len(other.components)))
+        if sc != oc:
+            return False
+        return all(getattr(self, attr) == getattr(other, attr)
+                   for attr in ['suffix', 'alpha', 'beta', 'pre',
+                                'rc', 'p', 'revision'])
+
+    def __lt__(self, other):
+        if not isinstance(other, Version):
+            return NotImplemented
+        # Treat missing components as zero
+        l = max(len(self.components), len(other.components))
+        sc = self.components + ((0,) * (l - len(self.components)))
+        oc = other.components + ((0,) * (l - len(other.components)))
+        if sc != oc:
+            return sc < oc
+        if self.suffix != other.suffix:
+            return self.suffix < other.suffix
+        for attr, sign in [('alpha', -1), ('beta', -1), ('pre', -1),
+                           ('rc', -1), ('p', 1), ('revision', 1)]:
+            if (sa := getattr(self, attr)) != (oa := getattr(other, attr)):
+                if sa is not None and oa is not None:
+                    return sa < oa
+                else:
+                    return sign*int(sa is not None) < sign*int(oa is not None)
+        # Here we have equality
+        return False
+
+    def __hash__(self):
+        # Be careful as we treat missing components as zero, so existing zeros
+        # need to be dropped
+        normalized_components = tuple(reversed(self.components))
+        while normalized_components and normalized_components[0] == 0:
+            normalized_components = normalized_components[1:]
+        data = (normalized_components) + tuple(
+            getattr(self, attr) for attr in ['suffix', 'alpha', 'beta', 'pre',
+                                             'rc', 'p', 'revision'])
+        return hash(data)
+
+    def serialize(self):
+        return str(self)
+
+    @classmethod
+    def deserialize(cls, value):
+        mo = re.fullmatch(
+            r'([0-9\.]+)([a-z])?(?:_alpha([0-9]+))?(?:_beta([0-9]+))?'
+            r'(?:_pre([0-9]+))?(?:_rc([0-9]+))?(?:_p([0-9]+))?(?:-r([0-9]+))?',
+            value)
+        (rawcomponents, rawsuffix, rawalpha, rawbeta, rawpre, rawrc, rawp,
+         rawrevision) = mo.groups()
+        components = tuple(map(int, rawcomponents.split('.')))
+        suffix = rawsuffix or ""
+        alpha = int(rawalpha) if rawalpha else None
+        beta = int(rawbeta) if rawbeta else None
+        pre = int(rawpre) if rawpre else None
+        rc = int(rawrc) if rawrc else None
+        p = int(rawp) if rawp else None
+        revision = int(rawrevision) if rawrevision else None
+        return Version(components, suffix, alpha, beta, pre, rc, p, revision)
